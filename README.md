a1ed62663a59be0d507938f578172c15bfdd8cfa
Ark4ne <gui.allegret@gmail.com>
2016-11-24 16:32:17 +0100

    Neutrino\Dotenv

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# dotenv
+# neutrion-dotenv
 Loads environment variables from `.env.php` to `getenv()`, `$_ENV` and `$_SERVER` automagically.
+
---
+++ tests/DotenvTest.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: xlzi590
+ * Date: 24/11/2016
+ * Time: 15:21
+ */
+class DotenvTest extends PHPUnit_Framework_TestCase
+{
+
+  public function testWithNoEnvFileInGivenPath()
+  {
+    $env    = $_ENV;
+    $server = $_SERVER;
+
+    $this->assertFalse(\Neutrino\Dotenv\Dotenv::load(__DIR__));
+
+    $this->assertEquals($env, $_ENV);
+    $this->assertEquals($server, $_SERVER);
+  }
+
+  public function testValidLoad()
+  {
+    $expected = [
+      'APP_ENV' => 'testing',
+      'KEY_1'   => 'TESTING_VALUE_1',
+      'KEY_2'   => 'VALUE_2',
+      'KEY_3'   => 'TESTING_VALUE_3',
+    ];
+
+    $path = __DIR__ . DIRECTORY_SEPARATOR . 'fixture' . DIRECTORY_SEPARATOR . 'testing';
+
+    $this->assertTrue(\Neutrino\Dotenv\Dotenv::load($path));
+
+    foreach ($expected as $key => $value) {
+      if (function_exists('putenv')) {
+        $this->assertEquals(getenv($key), $value);
+      }
+
+      $this->assertArrayHasKey($key, $_ENV);
+      $this->assertArrayHasKey($key, $_SERVER);
+
+      $this->assertEquals($_ENV[$key], $value);
+      $this->assertEquals($_SERVER[$key], $value);
+    }
+  }
+
+  /**
+   * @expectedException \Neutrino\Dotenv\Exception\InvalidFileException
+   */
+  public function testWrongValue()
+  {
+    $path = __DIR__ . DIRECTORY_SEPARATOR . 'fixture' . DIRECTORY_SEPARATOR . 'failing';
+
+    \Neutrino\Dotenv\Dotenv::load($path);
+  }
+}
